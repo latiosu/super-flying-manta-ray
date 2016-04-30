2bb03cef352b32787b468371b9df556827d6b263
Jonathan <Home@MBA.local>
2016-05-01 03:36:28 +1000

    Add more water and fix stopping condition

--- core/src/com/lasagne/game/Game.java
+++ core/src/com/lasagne/game/Game.java
@@ -1,23 +1,23 @@
 package com.lasagne.game;
 
-import java.util.Random;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+import java.util.Random;
 
 
 public class Game extends ApplicationAdapter {
@@ -110,11 +110,12 @@ public class Game extends ApplicationAdapter {
             }
         });
 
+        addDialogs();
     }
 
 	@Override
 	public void render () {
-        if (player.y <= water.getY()) {
+        if (player.y <= 0) {
             camera_paused = true;
             exitGame();
         }
@@ -154,4 +155,6 @@ public class Game extends ApplicationAdapter {
     public void exitGame() {
         // do something
     }
+
+
 }
