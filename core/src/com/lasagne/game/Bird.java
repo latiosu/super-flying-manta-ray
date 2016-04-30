7977b1a2ec2b538e376d399cd27a92cb3662883b
latiosu <latiosworks@gmail.com>
2016-04-30 15:39:05 +1000

    no more box2d

--- core/src/com/lasagne/game/Bird.java
+++ core/src/com/lasagne/game/Bird.java
@@ -3,16 +3,12 @@ package com.lasagne.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.CircleShape;
-import com.badlogic.gdx.physics.box2d.Fixture;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
 
 public class Bird {
     Texture bird;
     Body body;
     boolean upMove;
-    int width, height, speed;
+    int width, height, speed, x, y, velX, velY, accX, accY;
 
     public Bird() {
         bird = new Texture("bird-solo.png");
@@ -21,32 +17,12 @@ public class Bird {
         height = (int) (Gdx.graphics.getHeight() * 0.3f);
         speed = 700;
 
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.DynamicBody; // Subject to gravity
-        bodyDef.position.set(Gdx.graphics.getWidth() / 2, 100);
-
-        // Create out body in the world using our body definition
-        body = Game.getWorld().createBody(bodyDef);
-
-        CircleShape circle = new CircleShape();
-        circle.setRadius(6f);
-
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = circle;
-        fixtureDef.density = 0.5f;
-        fixtureDef.friction = 0.4f;
-        fixtureDef.restitution = 0.0f; // No bouncy bird
-
-        // Attach fixture to the body
-        Fixture fixture = body.createFixture(fixtureDef);
-
-        // Always dispose of shapes when you finish with them!
-        circle.dispose();
     }
 
     public void updateMotion() {
         if (upMove) {
-            body.setLinearVelocity(0, speed * Game.PIXEL_SCALE * Gdx.graphics.getDeltaTime());
+            // Own physics code here
+            System.out.println(body.getPosition().x);
         }
     }
 
--- core/src/com/lasagne/game/Game.java
+++ core/src/com/lasagne/game/Game.java
@@ -6,22 +6,9 @@ import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.Body;
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.Box2D;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.PolygonShape;
-import com.badlogic.gdx.physics.box2d.World;
 
 public class Game extends ApplicationAdapter {
-    public static final int PIXEL_SCALE = 10000;
-
-    private static World world;
-
-    Box2DDebugRenderer box2dDebug;
 
     Camera camera;
 	SpriteBatch batch;
@@ -29,10 +16,6 @@ public class Game extends ApplicationAdapter {
 
 	@Override
 	public void create () {
-        Box2D.init();
-        world = new World(new Vector2(0, -10*PIXEL_SCALE), true); // Gravity + ignore inactive bodies
-        box2dDebug = new Box2DDebugRenderer();
-
         camera = new OrthographicCamera();
         batch = new SpriteBatch();
         player = new Bird();
@@ -52,19 +35,6 @@ public class Game extends ApplicationAdapter {
             }
         });
 
-        // Create the ground
-        BodyDef groundBodyDef = new BodyDef();
-        groundBodyDef.position.set(new Vector2(0, 10));
-
-        Body groundBody = world.createBody(groundBodyDef);
-
-        // Set the polygon shape as a box which is twice the size of our viewport
-        // (setAsBox takes half-width and half-height as arguments)
-        PolygonShape groundBox = new PolygonShape();
-        groundBox.setAsBox(camera.viewportHeight, 10.0f);
-        groundBody.createFixture(groundBox, 0.0f);
-        groundBox.dispose();
-
     }
 
 	@Override
@@ -73,18 +43,11 @@ public class Game extends ApplicationAdapter {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		batch.begin();
-        batch.draw(player.bird, player.body.getPosition().x, player.body.getPosition().y, player.width, player.height);
+        batch.draw(player.bird, player.x, player.y, player.width, player.height);
 		batch.end();
-
-        box2dDebug.render(world, camera.combined);
 
         // Update bird positions before rendering
         player.updateMotion();
 
-        world.step(1/45f, 6, 2);
 	}
-
-    public static World getWorld() {
-        return world;
-    }
 }
