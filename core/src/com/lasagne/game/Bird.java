3420840c84c1029b4e701d89b0fc93ec9ec71387
Jarryd <jste8953@uni.sydney.edu.au>
2016-04-30 16:59:47 +1000

    x velocity done!

--- core/src/com/lasagne/game/Bird.java
+++ core/src/com/lasagne/game/Bird.java
@@ -15,19 +15,33 @@ public class Bird {
         upMove = false;
         width = (int) (Gdx.graphics.getWidth() * 0.3f);
         height = (int) (Gdx.graphics.getHeight() * 0.3f);
-        x = 0;
-        y = 1000;
+        x = 500;
+        y = 800;
         accY = 1;
+        velX = 0;
     }
 
     public void updateMotion() {
         if (upMove) {
             // Own physics code here
-            System.out.println(y);
-            velY = -20;
+            // Check stingray hasn't left the screen
+            if (y > Gdx.graphics.getHeight()) {
+                velY = 0;
+            } else {
+                velY = -20;
+            }
+
+            velX = 3;
         } else {
             velY = velY + accY;
+            velX = -1;
         }
+        // Check stringray is within boundaries
+        if (x < 400 || x > 1600) {
+            velX = 0;
+        }
+        // Update x & y positions
+        x = x + velX;
         y = y - (velY + accY / 2);
     }
 
